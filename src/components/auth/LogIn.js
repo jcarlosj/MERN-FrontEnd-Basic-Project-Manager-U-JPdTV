96fd1939fe4583cb29c33720a22dbe0255a450bc
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-23 15:32:55 -0500

    [+] LogIn: Form

--- src/components/auth/LogIn.js
+++ src/components/auth/LogIn.js
@@ -2,7 +2,38 @@ import React from 'react';
 
 const LogIn = () => {
     return(
-        <h2>LogIn Component</h2>
+        <div className="login-form">
+            <div className="form-container shadow-dark">
+                <h1>Iniciar Sesión</h1>
+
+                <form>
+                    <div className="form-field">
+                        <label htmlFor="email">Correo electrónico</label>
+                        <input 
+                            id="email"
+                            type="email"
+                            name="email"
+                            placeholder="Ej: jcarlosj@correo.co"
+                        />
+                    </div>
+                    <div className="form-field">
+                        <label htmlFor="password">Contraseña</label>
+                        <input 
+                            id="password"
+                            type="password"
+                            name="password"
+                            placeholder="Procura usar una clave segura"
+                        />
+                    </div>
+                    <div className="form-field">
+                        <button
+                            type="button"
+                            className="btn btn-primary btn-block"
+                        >Iniciar Sesión</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
 
