04ec111b2165fd9984fab24d6cd5c629e6703a2c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-30 11:22:38 -0500

    [+] NewAndEditTask > ProjectProvider: Show task form only when a project is selected

--- src/components/tasks/NewAndEditTask.js
+++ src/components/tasks/NewAndEditTask.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+/** Contexts */
+import ProjectContext from '../../context/projects/project-context';
+
+/** Component */
 const NewAndEditTask = () => {
+
+    /** Get State Project Context */
+    const 
+        projectContext = useContext( ProjectContext ),           // Hace accesible los datos del State del Contexto
+        { project } = projectContext;                            // Destructuring Context Provider
+
+
+    /** Validates if there is no selected project */
+    if( ! project ) return null;
+    
+    const [ actualProject ] = project;                            // Array Destructuring
+
     return(
         <div className="new-and-edit-task-container">
             <form 
