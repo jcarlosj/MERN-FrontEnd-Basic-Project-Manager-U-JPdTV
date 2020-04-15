4e643cd1939d5f75e33e5d880f68a31673026755
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-04-15 15:44:44 -0500

    [+] AlertProvider: Add functionality to show/hide alert

--- src/context/alerts/alert-reducer.js
+++ src/context/alerts/alert-reducer.js
@@ -1,3 +1,4 @@
+import { SHOW_ALERT, HIDE_ALERT } from '../../types';     // No pongo nombre del archivo por que se llama 'index.js' y lo reconoce por defecto.
 /** Define las acciones o eventos del Componente 
  *      - state: Estado del Componente antes de algún cambio.
  *      - action: Un Objeto JavaScrit con uno o dos parámetros
@@ -6,6 +7,15 @@
 */
 const AlertReducer = ( state, action ) => {   
     switch( action .type ) {
+        // Acciones a ejecutar
+        case SHOW_ALERT:  
+            return {
+                alert: action .payload  // Show Alert
+            }
+        case HIDE_ALERT:  
+            return {
+                alert: null             // Hide Alert
+            }
         default:            // Acción por defecto
             return state;
     }
--- src/context/alerts/alert-state.js
+++ src/context/alerts/alert-state.js
@@ -4,6 +4,9 @@ import React, { useReducer } from 'react';
 import AlertContext from './alert-context';
 import AlertReducer from './alert-reducer';
 
+/** Types */
+import { SHOW_ALERT, HIDE_ALERT } from '../../types';       // No pongo nombre del archivo por que se llama 'index.js' y lo reconoce por defecto.
+
 /** Context Status */
 const AlertState = props => {
 
@@ -19,13 +22,34 @@ const AlertState = props => {
         initialState        // InitialState: Estado inicial del Componente
     );
 
+    /** Show Alert */
+    const showAlert = ( message, category ) => {
+        dispatch({
+            type: SHOW_ALERT,
+            payload: {
+                message,
+                category
+            }
+        });
+
+        /** Hide Alert */
+        setTimeout( () => {
+            dispatch({
+                type: HIDE_ALERT
+            });
+        }, 5000 );
+    }
+
     return(
         /** AlertContext .Provider:
          *  Provee de datos al Contexto pasando un objeto.
          *      - value: Objeto con los Valores o Funciones que estarán disponibles globalmente
         */
         <AlertContext .Provider
-            value={{}}
+            value={{
+                alert: state .alert,      // Valor del State
+                showAlert                 // Funcionalidad
+            }}
         >
             { props .children }         {/* Permite el paso de datos entre los componentes hijos anidados a este Provider */}
         </AlertContext .Provider>
