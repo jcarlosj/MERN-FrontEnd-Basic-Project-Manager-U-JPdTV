8726ef79e8fe65d1c8b31569e1d50fc477456dde
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-04-22 10:33:48 -0500

    [+] Header: Show authenticated user information

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useContext, useEffect, useDebugValue } from 'react';
+
+/** Context */
+import AuthContext from '../../context/auth/auth-context';
 
 const Header = () => {
+
+    /** Get State Alert Context */
+    const 
+        authContext = useContext( AuthContext ),           // Hace accesible los datos del State del Contexto
+        { user, getAuthenticatedUser } = authContext;      // Destructuring Context Provider
+
+    /** Tracking 'authenticated' 
+         *  En caso que el usuario se haya autenticado o registrado
+        */
+    useEffect( () => {
+        getAuthenticatedUser();
+    }, []);
+
     return(
         <header className="header">
-            <p className="user-name">Hola <span>Elisa María</span></p>
-
+            { user
+                ?   <p className="user-name">Hola <span>{ user .name }</span></p>
+                :   null
+            }
             <nav className="nav-principal">
                 <a href="#!">Cerrar sesión</a>
             </nav>
