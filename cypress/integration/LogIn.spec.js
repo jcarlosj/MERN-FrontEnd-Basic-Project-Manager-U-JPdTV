10e192e79141b0b130f170c0c0d75fdfa2968b92
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-16 11:24:19 -0500

    [+] Cypress: Define 'Verifica pantalla de inicio'

---
+++ cypress/integration/LogIn.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+describe( '<Login />', () => {
+
+    it( 'Verifica pantalla de inicio', () => {
+        cy.visit( 'http://localhost:3000/' );
+    } );
+
+    it( 'Verifica formulario', () => {
+        
+    } );
+
+});
