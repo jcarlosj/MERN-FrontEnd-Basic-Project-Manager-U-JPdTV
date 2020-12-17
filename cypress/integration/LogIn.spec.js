1077a6a9f86fc890bd6a106530b5a18da5d9d5f6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-17 18:17:04 -0500

    [+] Cypress > LogIn.test: Define 'Debe mostrar el mensaje de error Verifique que los datos ingresados son correctos'

--- cypress/integration/LogIn.spec.js
+++ cypress/integration/LogIn.spec.js
@@ -18,4 +18,35 @@ describe( '<LogIn />', () => {
     
     } );
 
+    it( 'Debe mostrar el mensaje de error "Verifique que los datos ingresados son correctos"', () => {
+        
+        /** Autenticacion de un usuario que no existe */
+        cy .get( '[data-cy="login-input-email"]' ) .type( 'ana.maria@correo.co' );
+        cy .get( '[data-cy="login-input-password"]' ) .type( '98765432' );
+
+        cy .get( '[data-cy=login-button-submit]' ) .click();
+
+        cy .get( '[data-cy=alert-error]' )
+            .should( 'exist' )
+            .invoke( 'text' )
+            .should( 'eq', 'Verifique que los datos ingresados son correctos' );
+
+        cy .get( '[data-cy=alert-error]' )
+            .should( 'have.class', 'alert-error' );
+
+        /** Autenticacion de un usuario que existe, pero su contrasena esta errada */
+        cy .get( '[data-cy="login-input-email"]' ) .clear() .type( 'eva.sofia@correo.co' );
+
+        cy .get( '[data-cy=login-button-submit]' ) .click();
+
+        cy .get( '[data-cy=alert-error]' )
+            .should( 'exist' )
+            .invoke( 'text' )
+            .should( 'eq', 'Verifique que los datos ingresados son correctos' );
+
+        cy .get( '[data-cy=alert-error]' )
+            .should( 'have.class', 'alert-error' );
+
+    } );
+
 } );
